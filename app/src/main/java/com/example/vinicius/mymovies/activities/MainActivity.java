a07208dcaa1f3097e71bfb942cba48320e0c0dc9
Vinicius Silva <viniciusjns@gmail.com>
2018-08-13 10:46:21 -0300

    first commit

---
+++ app/src/main/java/com/example/vinicius/mymovies/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.vinicius.mymovies.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vinicius.mymovies.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
